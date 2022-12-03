177928702132e1c12fe234a24a797070295f89e0
James Xalis <jamesxalis@icloud.com>
2022-12-02 17:57:52 -0700

    fixed server issues

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+function Header({ currentPage, handlePageChange }) {
+    return (
+        <header>
+            <Navbar isBordered variant='static' height={125}>
+                <Grid.Container wrap="wrap">
+                    <Grid xs>
+                        <Navbar.Brand>
+                            <Col>
+                                <Text b h1 size={30} >
+                                    Tristan Saragosa
+                                </Text>
+                                <Text>
+                                    Full Stack Web Developer
+                                </Text>
+                            </Col>
+                        </Navbar.Brand>
+                    </Grid>
+                    <Grid >
+                        <Navbar.Content enableCursorHighlight activeColor='primary' hideIn="xsMax" variant='highlight-solid-rounded' >
+                            <NavBar currentPage={currentPage} handlePageChange={handlePageChange} />
+                        </Navbar.Content>
+                    </Grid>
+                </Grid.Container>
+            </Navbar>
+        </header>
+    );
+}
+
+export default Header ();
---
+++ src/components/Projects/MainProjects/MainProjects.js
@@ -0,0 +1,69 @@
+import { Card, Grid, Text } from '@nextui-org/react';
+import cardBattle from '../../../images/Card-Battle.png'
+
+function MainProjects () {
+    return (
+        <>
+            <Grid.Container gap={2} justify='center'>
+                <Grid xs={16} md={3}>
+                        <Card isHoverable isPressable>
+                            <Card.Header>
+                                <Text h3>
+                                    ReelSearch
+                                </Text>
+                            </Card.Header>
+                            <Card.Divider/>
+                            <Card.Body>
+                                <Text>Being updated</Text>
+                                <Text>
+                                    HTML, JavaScript, CSS
+                                </Text>
+                            </Card.Body>
+                        </Card>
+                </Grid>
+                <Grid xs={16} md={3}>
+                    <Card isHoverable isPressable>
+                        <Card.Header css={{ position: "absolute", zIndex: 1, top: 100, left: 80 }}>
+                            <Text h3>
+                                Card Battle
+                            </Text>
+                        </Card.Header>
+                        <Card.Body css={{ position: "absolute", zIndex: 1, top: 160, left: 50 }}>
+                            <Text b>A Turn Based Card Game</Text>
+                            <Text b>
+                                Express, Sequelize,
+                            </Text>
+                            <Text b>
+                                JavaScript, Handlebars
+                            </Text>
+                        </Card.Body>
+                        <Card.Image
+                            objectFit='fill'
+                            alt='Card Game Project'
+                            width="100%"
+                            height={340}
+                        />
+                    </Card>
+                </Grid>
+                <Grid xs={16} md={3}>
+                    <Card isHoverable isPressable>
+                        <Card.Header>
+                            <Text h3>
+                                Coming Soon!
+                            </Text>
+                        </Card.Header>
+                        <Card.Divider/>
+                        <Card.Body>
+                            <Text>Coming Soon</Text>
+                            <Text>
+                                React, GraphQL, Mongoose, Express, JavaScript
+                            </Text>
+                        </Card.Body>
+                    </Card>
+                </Grid>
+            </Grid.Container>
+        </>
+    );
+}
+
+export default MainProjects;
