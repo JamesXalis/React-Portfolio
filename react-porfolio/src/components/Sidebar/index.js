fe81c243a801357c4e7d809eb0fd308cb51b3b23
James Xalis <jamesxalis@icloud.com>
2022-12-07 11:58:07 -0700

    added navbar and linkedin link

---
+++ react-porfolio/src/components/Sidebar/index.js
@@ -0,0 +1,37 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+// import logoX from '../../assets'
+// import logoSubtitle from '../../assets'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            {/* <img src={logoX} alt='logo' />
+            <img className="sub-logo" src={logoSubtitle} alt='logo' /> */}
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname='active' to='/'>
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname='active' className='about-link' to='/about'>
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname='active' className='contact-link' to='/contact'>
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/jamesxalis/'>
+                    <FontAwesomeIcon icon={faLinkedin} color='#a4d4d4e' />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
